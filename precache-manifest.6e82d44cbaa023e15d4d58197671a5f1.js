0a543fe005045bd485cea63ba7baa611514b6989
SamRong <YungSun3307@163.com>
2021-02-28 14:26:49 +0800

    deploy

---
+++ precache-manifest.6e82d44cbaa023e15d4d58197671a5f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a100fcde3c08a51fe34e0018dd515bfb",
+    "url": "/tally-website/QR-Code.png"
+  },
+  {
+    "revision": "8b14749188bb41e2244e",
+    "url": "/tally-website/css/app.31b3f21b.css"
+  },
+  {
+    "revision": "2321dc98b6f9d0746aacace73b7c81f6",
+    "url": "/tally-website/fonts/Montserrat-Light.2321dc98.otf"
+  },
+  {
+    "revision": "91696e0984cfab74ff344325e5015da5",
+    "url": "/tally-website/fonts/Montserrat.91696e09.otf"
+  },
+  {
+    "revision": "d60c0d3151a72d9244107d50a49dbe22",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "8b14749188bb41e2244e",
+    "url": "/tally-website/js/app.0c2123f2.js"
+  },
+  {
+    "revision": "e6611fdd53466bd8f7c5",
+    "url": "/tally-website/js/chunk-vendors.983e70df.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  }
+]);
